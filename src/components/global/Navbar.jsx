f96af3969924d21fc81ef9e6034b89b4a137b8f9
shakil89427 <shakilahmed89427@gmail.com>
2022-11-13 12:29:01 +0600

    little change

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -17,15 +17,15 @@ const Navbar = () => {
 
   return (
     <div
-      className={`sticky top-0 bg-black text-white z-10 border-gray-600 ${
-        showBorder ? "border-b" : "border-0"
+      className={`sticky top-0 bg-black text-white z-10 ${
+        showBorder ? "border-b border-gray-500" : "border-0"
       }`}
     >
-      <div className="h-14 sm:h-16 md:h-20 flex items-center justify-center sm:justify-between gap-5 sm:max-w-[680px] md:max-w-[808px] lg:max-w-[1064px] xl:max-w-[1320px] 2xl:max-w-[1576px] mx-auto px-2">
+      <div className="h-14 sm:h-16 md:h-20 flex items-center justify-between gap-5 sm:max-w-[680px] md:max-w-[808px] lg:max-w-[1064px] xl:max-w-[1320px] 2xl:max-w-[1576px] mx-auto px-2">
         <img
           src={bevysquarewhite}
           alt="bevysquareWhite"
-          className="w-40 sm:w-44 md:w-48 lg:w-52 xl:w-56"
+          className="w-44 sm:w-48 md:w-52 lg:w-56 xl:w-60"
         />
         <button className="border rounded-md px-5 py-2 text-sm sm:text-md lg:text-lg bg-white text-black max-h-[48px] font-medium uppercase hidden sm:block">
           Contact us
