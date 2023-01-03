aca57c03b3e568009e23d73233e32d33b9376b5f
shakil89427 <shakilahmed89427@gmail.com>
2023-01-04 00:28:48 +0600

    buffer disabled

--- server/src/helpers/connectDB.js
+++ server/src/helpers/connectDB.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   mongoose.set("strictQuery", true);
+  mongoose.set("bufferCommands", false);
   mongoose
-    .connect(process.env.DATABASE_URI, { family: 4 })
+    .connect(process.env.DATABASE_URI, {
+      family: 4,
+      serverSelectionTimeoutMS: 5000,
+    })
     .catch((error) => console.log(error?.message));
   mongoose.connection.on("connected", () => console.log("DB connected"));
   mongoose.connection.on("disconnected", () => console.log("DB disconnected"));
