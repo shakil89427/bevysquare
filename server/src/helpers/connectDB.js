adc4c9469b46ddcc4a5a8060690fa34d47ff0e25
shakil89427 <shakilahmed89427@gmail.com>
2023-01-11 17:58:06 +0600

    checked

--- server/src/helpers/connectDB.js
+++ server/src/helpers/connectDB.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 module.exports = () => {
   mongoose.set("strictQuery", true);
   mongoose
-    .connect(process.env.DATABASE_URI, { family: 4 })
+    .connect(process.env.DATABASE_URI, {
+      family: 4,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .catch((error) => console.log(error?.message));
   mongoose.connection.on("connected", () => console.log("DB connected"));
   mongoose.connection.on("disconnected", () => console.log("DB disconnected"));
